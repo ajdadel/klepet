39ad8a64cb6d3660820ace58d880d8fc88ab0f7d
Ajda Delic <ajda.delic@gmail.com>
2016-04-10 21:15:14 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -93,7 +93,17 @@ $(document).ready(function() {
       $('#poslji-sporocilo').focus();
     });
   });
-
+  
+  socket.on('dregljaj', function() {
+    $('#vsebina').jrumble();
+    $('#vsebina').trigger('startRumble');
+    
+    setTimeout(function(){ 
+      $('#vsebina').trigger('stopRumble');
+    }, 1500);
+      
+  });
+     
   socket.on('uporabniki', function(uporabniki) {
     $('#seznam-uporabnikov').empty();
     for (var i=0; i < uporabniki.length; i++) {
